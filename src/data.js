da09b0ccbfab02ee173f2d854f5bef0d7513797c
pavel11123 <korobko.11@inbox.ru>
2022-07-21 18:21:30 +0600

    init

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export default [
+  { id: "1", imageName: "content-photo-1.webp", tag: "кухни" },
+  { id: "2", imageName: "content-photo-2.webp", tag: "шкафы и тумбы" },
+  { id: "3", imageName: "content-photo-3.webp", tag: "кухни" },
+  { id: "4", imageName: "content-photo-4.webp", tag: "шкафы и тумбы" },
+  { id: "5", imageName: "content-photo-5.webp", tag: "кухни" },
+  { id: "6", imageName: "content-photo-6.webp", tag: "шкафы и тумбы" },
+  { id: "7", imageName: "content-photo-7.webp", tag: "кухни" },
+  { id: "8", imageName: "content-photo-8.webp", tag: "шкафы и тумбы" },
+  { id: "9", imageName: "content-photo-9.webp", tag: "шкафы и тумбы" },
+];
